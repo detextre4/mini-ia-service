e86d9d09ff50bdfb4763ca077d1d18265b3032f0
detextre4 <detextre4@gmail.com>
2024-07-01 11:29:59 -0400

    config/router-generation

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const app = require('express')(),
+const express = require('express'),
 fs = require('fs'),
+path = require('path'),
+
+app = express(),
 port = 3000;
 
 require('dotenv').config();
 
 fs.readdirSync('./routes').forEach(file => {
-  const route = require(`./routes/${file}`),
-  path = `/${file.split('.')[0]}`;
+  const fileName = path.basename(file, '.js'),
+  routePath = `/${fileName}`;
 
-  app.use(path, route);
+  if (fs.statSync(`./routes/${file}`).isDirectory()) {
+    const folderPath = `./routes/${file}`,
+    filesInFolder = fs.readdirSync(folderPath);
+
+    filesInFolder.forEach(subFile => {
+      const subFileName = path.basename(subFile, '.js'),
+      subRoutePath = `/${fileName}/${subFileName}`,
+      subRoute = require(`${folderPath}/${subFile}`);
+
+      app.use(subRoutePath, subRoute);
+    });
+  } else {
+    const route = require(`./routes/${file}`);
+    app.use(routePath, route);
+  }
 });
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
